5b5ea46c6966d9fdca60f4303110230c2272dc06
v1572a <vamshi16n@gmail.com>
2024-02-15 14:52:31 +0530

    Mongoose async

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const app=express()
 const dotenv=require('dotenv')
 const {requireAuthen}=require('./Authmiddleware/authMiddleware')
 dotenv.config()
+
+
 const cors=require('cors')
 const URI=process.env.URI;
-mongoose.connect(URI).then(console.log("Connected to MongoDB"))
-.catch((err)=>console.log(err))
-const db=mongoose.connection
+
+const run = async () => {
+  await mongoose.connect(URI);
+  console.log("Connected to myDB");
+}
+
+run()
+.catch((err) => console.error(err))
+// mongoose.connect(URI).then(console.log("Connected to MongoDB"))
+// .catch((err)=>console.log(err))
+// const db=mongoose.connection
  
 app.use(cors({
     origin: 'http://localhost:3000',
@@ -18,7 +28,6 @@ app.use(cors({
 app.use(bodyParser.json())
 //  error check
  app.use((req, res,next) => {
-   console.log(1)
   console.log("\nNew Request Made :");
   console.log("Host : ", req.hostname);
   console.log("Path : ", req.path);
